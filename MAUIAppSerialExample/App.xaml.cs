d950d3240d6ed577c74b9c2df7fa315c4e629273
MOtterbine <mikeotterbine@gmail.com>
2025-03-28 16:21:43 -0400

    Add documents and Android permissions

--- MAUIAppSerialExample/App.xaml.cs
+++ MAUIAppSerialExample/App.xaml.cs
@@ -1,30 +1,47 @@
-﻿namespace MAUIAppSerialExample
+﻿namespace MAUIAppSerialExample;
+
+public delegate void PermissionsResultReady(object sender, EventArgs e);
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public bool HasPermissions { get; set; } = false;
+    public event PermissionsResultReady PermissionsReadyEvent;
+    public void FirePermissionsReadyEvent()
     {
-        public App()
-        {
-            InitializeComponent();
 
-            MainPage = new AppShell();
+        if (this.PermissionsReadyEvent != null)
+        {
+            this.PermissionsReadyEvent(null, EventArgs.Empty);
         }
+    }
 
-        protected override Window CreateWindow(IActivationState activationState)
-        {
+    public App()
+    {
 
-            var window = base.CreateWindow(activationState);
+#if WINDOWS
+        this.HasPermissions = true;
+#endif
+
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+
+        var window = base.CreateWindow(activationState);
 
 #if WINDOWS
 
-            window.MinimumWidth = Constants.MIN_WINDOW_WIDTH_WINDOWS;
-            window.MinimumHeight = Constants.MIN_WINDOW_HEIGHT_WINDOWS;
+        window.MinimumWidth = Constants.MIN_WINDOW_WIDTH_WINDOWS;
+        window.MinimumHeight = Constants.MIN_WINDOW_HEIGHT_WINDOWS;
 
 #endif
 
-            return window;
-
-        }
-
+        return window;
 
     }
+
+
 }
