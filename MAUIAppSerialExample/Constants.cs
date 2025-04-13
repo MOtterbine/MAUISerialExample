23a96c081344435c8bf2d82fa838e29ada7a39c4
MOtterbine <mikeotterbine@gmail.com>
2025-04-13 13:13:51 -0400

    Fix spelling error. Fix: allow send button to resize

--- MAUIAppSerialExample/Constants.cs
+++ MAUIAppSerialExample/Constants.cs
@@ -10,7 +10,7 @@ public class Constants
     public const int DEFAULT_COMM_NO_RESPONSE_TIMEOUT =3500;
     public const int MIN_WINDOW_HEIGHT_WINDOWS = 780;
     public const int MIN_WINDOW_WIDTH_WINDOWS = 650;
-    public const String DEVICE_NO_RESPONSE = "<No Respone>";
+    public const String DEVICE_NO_RESPONSE = "<No Response>";
     public const String SERIAL_DEVICE_NAME = "OBDII";
     public const String DEVICE_NOT_SETUP = "** Device Not Set ***";
     public const String DEVICE_NOT_CONNECTED = "** Device is not connected or invalid ***";
